25250617281653b6c3bd553056ea275b1b459305
kushagra sharma <69472040+C-Y-P-H-E-R@users.noreply.github.com>
2021-12-17 21:59:05 +0530

    day-1 added.

---
+++ Java-in-30-days/day-1.java
@@ -0,0 +1,19 @@
+// used java 15
+
+import java.io.*;
+import java.util.*;
+
+// everything is a class or object in Java.
+// work is done using methods.
+// Scanner is used to create an input stream.
+// System class is used to create an output stream and give output
+// nextInt() method for int and nextLine() method for Strings
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(str);
+    }
+}
